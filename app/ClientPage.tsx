00e5efdfa52c77f7f04a623ffba34ddd09e33dce
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-15 12:15:39 +0900

    Add code editor

--- app/ClientPage.tsx
+++ app/ClientPage.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import { ReactElement } from "react";
+import { ReactElement, useRef, useState } from "react";
 
 export function ClientPage(): ReactElement | null {
+  const [code, setCode] = useState<string>("");
+  const [snippetName, setSnippetName] = useState<string>("");
+  const textarea = useRef<HTMLTextAreaElement>(null);
   return (
     <div
       className="flex flex-col items-center justify-center min-h-screen bg-gray-100"
@@ -12,6 +15,47 @@ export function ClientPage(): ReactElement | null {
       >
         Ruby CPS Visualizer
       </h1>
+      <select
+        className="mt-4 p-2 border border-gray-300 rounded"
+        value={snippetName}
+        onChange={(e) => {
+          const name: string = e.currentTarget.value;
+          setSnippetName(name);
+          const snip = codeExamples.find((example) => example.name === name);
+          if (snip) {
+            setCode(snip.code);
+            if (textarea.current) {
+              textarea.current.value = snip.code;
+            }
+          }
+        }}
+      >
+        <option value="">Select a code snippet</option>
+        {codeExamples.map((example) => (
+          <option key={example.name} value={example.name}>
+            {example.name}
+          </option>
+        ))}
+      </select>
+      <textarea
+        ref={textarea}
+        className="mt-4 p-2 border border-gray-300 rounded w-1/2 h-64"
+        onChange={(e) => {
+          setCode(e.currentTarget.value);
+          setSnippetName("");
+        }}
+      />
     </div>
   );
 }
+
+type CodeSnippet = {
+  name: string;
+  code: string;
+};
+const codeExamples: CodeSnippet[] = [
+  {
+    name: "Add",
+    code: "p 1 + 2",
+  },
+];
