785c82f51869bd353b371346e93b566af297eeae
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-16 09:37:26 +0900

    Initial call stringification

--- app/stringify.ts
+++ app/stringify.ts
@@ -1,4 +1,4 @@
-import { IntegerNode, Node, ProgramNode, StatementsNode } from "@ruby/prism";
+import { CallNode, IntegerNode, Node, ProgramNode, StatementsNode } from "@ruby/prism";
 
 export class StringifyError extends Error {
   static {
@@ -13,6 +13,9 @@ export function stringifyProgram(program: ProgramNode): string {
 }
 
 const INDENT_UNIT = "  ";
+const LEVEL_PRIMARY = 1;
+const LEVEL_CALL = 2;
+const LEVEL_STMT = 3;
 
 class Printer {
   indent: number = 0;
@@ -29,13 +32,63 @@ class Printer {
   }
 
   printStatement(statement: Node): void {
-    if (statement instanceof IntegerNode) {
-      this.buf += statement.value.toString();
+    this.printExpression(statement, LEVEL_STMT);
+    this.print("\n");
+  }
+
+  printExpression(expression: Node, level: number): void {
+    const innerLevel = this.levelOfExpression(expression);
+    if (innerLevel > level) {
+      this.print("(");
+      this.printExpression(expression, innerLevel);
+      this.print(")");
+      return;
+    }
+    if (expression instanceof IntegerNode) {
+      this.buf += expression.value.toString();
+    } else if (expression instanceof CallNode) {
+      if (expression.receiver) {
+        this.printExpression(expression.receiver, LEVEL_CALL);
+        if (expression.isSafeNavigation()) {
+          this.print("&.");
+        } else {
+          this.print(".");
+        }
+      }
+      this.print(expression.name);
+      if (expression.arguments_) {
+        this.print("(");
+        let first = true;
+        for (const arg of expression.arguments_.arguments_) {
+          if (!first) {
+            this.print(", ");
+          }
+          first = false;
+          this.printArgument(arg);
+        }
+        this.print(")");
+      }
     } else {
       throw new StringifyError(
-        `Unsupported statement type: ${statement.constructor.name}`
+        `Unsupported expression type: ${expression.constructor.name}`
       );
     }
+  }
+
+  levelOfExpression(expression: Node): number {
+    if (expression instanceof IntegerNode) {
+      return LEVEL_PRIMARY;
+    } else if (expression instanceof CallNode) {
+      return LEVEL_CALL;
+    } else {
+      throw new StringifyError(
+        `Unsupported expression type: ${expression.constructor.name}`
+      );
+    }
+  }
+
+  printArgument(arg: Node): void {
+    this.printExpression(arg, LEVEL_STMT);
   }
 
   print(text: string): void {
