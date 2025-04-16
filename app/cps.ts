f4517c3bad16a8f8e2556d31128f12e116d33569
Masaki Hara <ackie.h.gmai@gmail.com>
2025-04-16 12:19:28 +0900

    Rework cps

--- app/cps.ts
+++ app/cps.ts
@@ -1,4 +1,4 @@
-import { ArgumentsNode, BeginNode, BlockNode, CallNode, LocalVariableReadNode, Location, NilNode, Node, ParenthesesNode, ProgramNode, StatementsNode } from "@ruby/prism";
+import { ArgumentsNode, BeginNode, BlockArgumentNode, BlockNode, BlockParametersNode, CallNode, LocalVariableReadNode, Location, NilNode, Node, ParametersNode, ParenthesesNode, ProgramNode, RequiredParameterNode, StatementsNode } from "@ruby/prism";
 
 export class CPSError extends Error {
   static {
@@ -7,106 +7,277 @@ export class CPSError extends Error {
 }
 
 export function cpsProgram(program: ProgramNode): ProgramNode {
-  const cont: Cont = (result: Node | null) => new CallNode(
-    0,
-    DUMMY_LOCATION,
-    0,
-    new LocalVariableReadNode(
+  return new CPSTransformer().cpsProgram(program);
+}
+
+type Hole<T> = (value: T) => void;
+
+class CPSTransformer {
+  intermiediateVariableCounter: number = 1;
+  contVariableCounter: number = 1;
+
+  cpsProgram(program: ProgramNode): ProgramNode {
+    const result = new ProgramNode(
       0,
       DUMMY_LOCATION,
       0,
-      "cont",
-      0
-    ),
-    null,
-    "call",
-    null,
-    null,
-    new ArgumentsNode(
-      0,
-      DUMMY_LOCATION,
-      0,
-      [result ?? new NilNode(0, DUMMY_LOCATION, 0)]
-    ),
-    null,
-    null
-  );
-  const statementsCont = cpsStatements(program.statements, cont);
-  const expr = statementsCont(null);
-  return new ProgramNode(
-    0,
-    program.location,
-    0,
-    program.locals,
-    new StatementsNode(
-      0,
-      program.location,
-      0,
-      [expr]
-    ),
-  );
-}
-
-type Cont = (result: Node | null) => Node;
-
-function cpsStatements(statements: StatementsNode, cont: Cont): Cont {
-  if (statements.body.length === 0) {
-    return cont;
-  }
-  let currentCont: Cont = cont;
-  for (const statement of statements.body.reverse()) {
-    currentCont = cpsExpression(statement, currentCont);
-  }
-  return currentCont;
-}
-
-function cpsExpression(expression: Node, cont: Cont): Cont {
-  if (expression instanceof StatementsNode) {
-    return cpsStatements(expression, cont);
-  } else if (expression instanceof ParenthesesNode) {
-    if (expression.body) {
-      return cpsExpression(expression.body, cont);
-    }
-  } else if (expression instanceof BeginNode) {
-    if (
-      expression.statements &&
-      !expression.rescueClause &&
-      !expression.elseClause &&
-      !expression.ensureClause
-    ) {
-      return cpsStatements(expression.statements, cont);
-    }
-  }
-  return (result: Node | null) =>
-    result
-      ? new CallNode(
+      [],
+      new StatementsNode(
+        0,
+        DUMMY_LOCATION,
+        0,
+        [dummyNode()],
+      ),
+    );
+    const initialHole: Hole<Node> = (value: Node) => {
+      result.statements.body[0] = value;
+    };
+    const [finalHole, finalVar] = this.cpsStatements(program.statements, initialHole, true);
+    finalHole(
+      new CallNode(
+        0,
+        DUMMY_LOCATION,
+        0,
+        new LocalVariableReadNode(
           0,
           DUMMY_LOCATION,
           0,
-          result,
-          null,
-          "then",
-          null,
-          null,
-          null,
-          null,
-          new BlockNode(
+          "cont",
+          0
+        ),
+        null,
+        "call",
+        null,
+        null,
+        new ArgumentsNode(
+          0,
+          DUMMY_LOCATION,
+          0,
+          [new LocalVariableReadNode(
             0,
             DUMMY_LOCATION,
             0,
-            [],
-            null,
-            new StatementsNode(
+            finalVar,
+            0
+          )]
+        ),
+        null,
+        null
+      )
+    );
+    return result;
+  }
+
+  cpsStatements(
+    statements: StatementsNode,
+    givenHole: Hole<Node>,
+    needResult: boolean
+  ): [Hole<Node>, string] {
+    const [init, last] =
+      needResult
+        ? [
+            statements.body.slice(0, statements.body.length - 1),
+            statements.body[statements.body.length - 1] ?? new NilNode(0, DUMMY_LOCATION, 0),
+          ]
+        : [statements.body, new NilNode(0, DUMMY_LOCATION, 0)];
+    let hole = givenHole;
+    for (const statement of init) {
+      const [newHole] = this.cpsExpression(statement, hole, false);
+      hole = newHole;
+    }
+    if (needResult) {
+      return this.cpsExpression(last, hole, true);
+    } else {
+      return [hole, ""];
+    }
+  }
+
+  cpsExpression(
+    expression: Node,
+    givenHole: Hole<Node>,
+    needResult: boolean,
+  ): [Hole<Node>, string] {
+    if (expression instanceof StatementsNode) {
+      return this.cpsStatements(expression, givenHole, needResult);
+    } else if (expression instanceof ParenthesesNode) {
+      if (expression.body) {
+        return this.cpsExpression(expression.body, givenHole, needResult);
+      }
+    } else if (expression instanceof BeginNode) {
+      if (
+        expression.statements &&
+        !expression.rescueClause &&
+        !expression.elseClause &&
+        !expression.ensureClause
+      ) {
+        return this.cpsStatements(expression.statements, givenHole, needResult);
+      }
+    } else if (expression instanceof CallNode) {
+      if (!(expression.block instanceof BlockNode)) {
+        let hole = givenHole;
+        let receiver: Node | null = null;
+        if (expression.receiver) {
+          const [newHole, varName] = this.cpsExpression(expression.receiver, hole, true);
+          hole = newHole;
+          receiver = new LocalVariableReadNode(
+            0,
+            DUMMY_LOCATION,
+            0,
+            varName,
+            0
+          );
+        }
+        const args: Node[] = [];
+        if (expression.arguments_) {
+          for (const arg of expression.arguments_.arguments_) {
+            const [newHole, varName] = this.cpsExpression(arg, hole, true);
+            hole = newHole;
+            args.push(new LocalVariableReadNode(
               0,
               DUMMY_LOCATION,
               0,
-              [cont(expression)],
-            ),
+              varName,
+              0
+            ));
+          }
+        }
+        let blockArg: Node | null = null;
+        if (expression.block instanceof BlockArgumentNode) {
+          const blockArgName = this.freshIntermediate();
+          blockArg = new LocalVariableReadNode(
+            0,
             DUMMY_LOCATION,
-            DUMMY_LOCATION
+            0,
+            blockArgName,
+            0
+          );
+          hole = (value: Node) => {
+            ((expression.block as BlockNode).body as StatementsNode).body[0] = value;
+          };
+        }
+        const varName = needResult ? this.freshIntermediate() : null;
+        const e = thenCall(
+          new CallNode(
+            0,
+            DUMMY_LOCATION,
+            0,
+            receiver,
+            null,
+            expression.name,
+            null,
+            null,
+            new ArgumentsNode(
+              0,
+              DUMMY_LOCATION,
+              0,
+              args
+            ),
+            null,
+            blockArg,
           ),
-        )
-      : cont(expression);
+          varName,
+          dummyNode(),
+        );
+        hole(e);
+        hole = (value: Node) => {
+          ((e.block as BlockNode).body as StatementsNode).body[0] = value;
+        };
+        return [hole, varName ?? ""];
+      }
+    }
+    return this.cpsExpressionFallback(expression, givenHole, needResult);
+  }
+
+  cpsExpressionFallback(
+    expression: Node,
+    givenHole: Hole<Node>,
+    needResult: boolean,
+  ): [Hole<Node>, string] {
+    const varName = needResult ? this.freshIntermediate() : null;
+    const e = thenCall(
+      expression,
+      varName,
+      dummyNode(),
+    );
+    givenHole(e);
+    const hole: Hole<Node> = (value: Node) => {
+      ((e.block as BlockNode).body as StatementsNode).body[0] = value;
+    };
+    return [hole, varName ?? ""];
+  }
+
+  freshIntermediate(): string {
+    return `e${this.intermiediateVariableCounter++}`;
+  }
+  freshContVar(): string {
+    return `c${this.contVariableCounter++}`;
+  }
+}
+
+function thenCall(
+  recv: Node,
+  local: string | null,
+  body: Node
+): CallNode {
+  return new CallNode(
+    0,
+    DUMMY_LOCATION,
+    0,
+    recv,
+    null,
+    "then",
+    null,
+    null,
+    null,
+    null,
+    new BlockNode(
+      0,
+      DUMMY_LOCATION,
+      0,
+      [],
+      local == null
+        ? null
+        : new BlockParametersNode(
+            0,
+            DUMMY_LOCATION,
+            0,
+            new ParametersNode(
+              0,
+              DUMMY_LOCATION,
+              0,
+              [
+                new RequiredParameterNode(
+                  0,
+                  DUMMY_LOCATION,
+                  0,
+                  local
+                ),
+              ],
+              [],
+              null,
+              [],
+              [],
+              null,
+              null
+            ),
+            [],
+            null,
+            null
+          ),
+      new StatementsNode(
+        0,
+        DUMMY_LOCATION,
+        0,
+        [body],
+      ),
+      DUMMY_LOCATION,
+      DUMMY_LOCATION
+    ),
+  )
+}
+
+function dummyNode(): Node {
+  return new NilNode(0, DUMMY_LOCATION, 0);
 }
 
 const DUMMY_LOCATION: Location = {
